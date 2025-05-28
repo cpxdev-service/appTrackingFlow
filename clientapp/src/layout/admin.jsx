2b4318d27208f7d4b687271226774b6726ac2ad5
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2025-05-28 15:31:00 +0700

    update new admin layout

--- clientapp/src/layout/admin.jsx
+++ clientapp/src/layout/admin.jsx
@@ -2,9 +2,106 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
 import { setMainLoad, setLoginSession } from "../redux/action";
 import axios from "axios";
+import React from "react";
+import Swal from "sweetalert2";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
+
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import CodeIcon from "@mui/icons-material/Code";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+
+const drawerWidth = 240;
 
 const AdminLayout = () => {
   const his = useNavigate();
+
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [isClosing, setIsClosing] = React.useState(false);
+
+  const handleDrawerClose = () => {
+    setIsClosing(true);
+    setMobileOpen(false);
+  };
+
+  const handleDrawerTransitionEnd = () => {
+    setIsClosing(false);
+  };
+
+  const handleDrawerToggle = () => {
+    if (!isClosing) {
+      setMobileOpen(!mobileOpen);
+    }
+  };
+
+  const drawer = (
+    <div>
+      <List>
+        <ListItem
+          disablePadding
+          color="primary"
+          sx={{
+            flexGrow: 1,
+            backgroundColor: "#1976d2",
+            display: { xs: "none", sm: "initial" },
+          }}>
+          <ListItemButton>
+            <ListItemText className="text-center m-2">
+              <b>App Tracking Flow</b>
+            </ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SpaceDashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AccountTreeIcon />
+            </ListItemIcon>
+            <ListItemText primary="App Flow" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <CodeIcon />
+            </ListItemIcon>
+            <ListItemText primary="API Access" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SupportAgentIcon />
+            </ListItemIcon>
+            <ListItemText primary="Support" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </div>
+  );
 
   const onSignout = () => {
     setMainLoad(true);
@@ -30,24 +127,118 @@ const AdminLayout = () => {
       });
   };
 
+  // return (
+  //   <div className="main-layout">
+  //     <header>
+  //       <h1>Welcome to the Main Layout</h1>
+  //       <button
+  //         className="btn btn-outline-primary"
+  //         onClick={() => onSignout()}
+  //         type="button">
+  //         Signout
+  //       </button>
+  //     </header>
+  //     <main style={{ marginTop: 80 }}>
+  //       <Outlet />
+  //     </main>
+  //     <footer className="text-center mt-5">
+  //       <p>&copy; {new Date().getFullYear()} CPXDev Studio</p>
+  //     </footer>
+  //   </div>
+  // );
   return (
-    <div className="main-layout">
-      <header>
-        <h1>Welcome to the Main Layout</h1>
-        <button
-          className="btn btn-outline-primary"
-          onClick={() => onSignout()}
-          type="button">
-          Signout
-        </button>
-      </header>
-      <main style={{ marginTop: 80 }}>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            size="large"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, visibility: { xs: "initial", sm: "hidden" } }}>
+            <b>App Tracking Flow</b>
+          </Typography>
+          <Button
+            color="inherit"
+            className="mt-1"
+            onClick={() => {
+              Swal.fire({
+                title: "Do you want to Signout",
+                showDenyButton: true,
+                confirmButtonText: "Confirm",
+                denyButtonText: `No`,
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  onSignout();
+                }
+              });
+            }}>
+            Signout
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onTransitionEnd={handleDrawerTransitionEnd}
+          onClose={handleDrawerClose}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          slotProps={{
+            root: {
+              keepMounted: true, // Better open performance on mobile.
+            },
+          }}>
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open>
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}>
+        <Toolbar />
         <Outlet />
-      </main>
-      <footer className="text-center mt-5">
-        <p>&copy; {new Date().getFullYear()} CPXDev Studio</p>
-      </footer>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
